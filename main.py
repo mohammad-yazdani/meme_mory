77182fd25de9c4cba4e9efdafcb868d1658da838
Ahoo <ahooom.z@gmail.com>
2019-03-10 21:08:36 -0400

    first commit from ahoo

---
+++ main.py
@@ -0,0 +1,11 @@
+params=list(input( ).split(" "))
+print(params)
+
+
+class meme:
+        def __init__(file, emotion_1, emotion_2, emotion_3, emotion_4):
+                    file.emotion_1 = emotion_1
+                            file.emotion_2 = emotion_2
+                                    file.emotion_3 = emotion_3
+                                            file.emotion_4 = emotion_4
+
