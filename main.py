7bc4405e393f34a15d15716b1fb8bf274464e1e7
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2019-03-10 21:38:18 -0400

    db half

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-params=list(input( ).split(" "))
-print(params)
+from store.db import DB
+from model.meme import Meme
 
-
-class meme:
-        def __init__(file, emotion_1, emotion_2, emotion_3, emotion_4):
-                    file.emotion_1 = emotion_1
-                            file.emotion_2 = emotion_2
-                                    file.emotion_3 = emotion_3
-                                            file.emotion_4 = emotion_4
-
+DB.start()
+Meme("test.png")
